096a73bcbd654696388251585b9aadb6e212bd52
AbdouAoufi <Abdouou7@gmail.com>
2021-08-23 10:57:25 +0100

    adding validatoin graphql validator

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,8 +1,21 @@
 const bcrypt = require("bcryptjs");
 const User = require("../models/User");
+const validator = require("validator");
 
-exports.signup  = {
+exports.signup = {
   createUser: async function ({ userInput }, req) {
+    const errors = [];
+    if (!validator.isEmail(userInput.email)) {
+      errors.push({ message: "email is not valid" });
+    }
+    if(validator.isEmpty(userInput.password) || !validator.isLength(userInput.password , {min : 5})){
+      errors.push({ message: "password is not valid" });
+    }
+
+    if(errors.length > 0 ) {
+      const error = new Error("Invalid input");
+      throw error;
+    }
     const existingUser = await User.findOne({ email: userInput.email });
     if (existingUser) {
       const error = new Error("User already exist!!");
@@ -24,7 +37,7 @@ exports.signup  = {
 };
 
 exports.operation = {
-  resolveOperation: ({userInput} , req) => {
-    return userInput.num1 + userInput.num2 ;
- }
-}
+  resolveOperation: ({ userInput }, req) => {
+    return userInput.num1 + userInput.num2;
+  },
+};
